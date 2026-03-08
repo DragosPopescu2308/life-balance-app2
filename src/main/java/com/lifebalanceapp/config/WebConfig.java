cd28a84e025a1e9ba5842a915eb2a6d10cb6bdd1
Dragos Popescu <dragospopescu74@yahoo.com>
2026-03-08 21:19:02 +0200

    allow vercel frontend

--- src/main/java/com/lifebalanceapp/config/WebConfig.java
+++ src/main/java/com/lifebalanceapp/config/WebConfig.java
@@ -11,7 +11,9 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/api/**")
                 .allowedOrigins(
                         "http://localhost:5173",
-                        "http://127.0.0.1:5173"
+                        "http://127.0.0.1:5173",
+                        "https://life-balance-app2.vercel.app"
+
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
